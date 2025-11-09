01a2302f3f0108dd737230a87645fd372a52c68a
Ahn Jung-min <dkswjdals4027@gmail.com>
2025-11-09 18:40:38 +0900

    Feat: Import navigation types and add Naver API URL

--- src/feature/NavigationService.ts
+++ src/feature/NavigationService.ts
@@ -1,4 +1,13 @@
 import axios from "axios";
+import {
+  Coordinates,
+  RouteInfo,
+  NcpDirectionsApiResponse, // 네이버 타입으로 변경
+} from "../types/navigation.types";
+
+// 네이버 Directions 5 API 엔드포인트
+const NAVER_DIRECTIONS_API_URL =
+  "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving";
 
 class NavigationService {
 
