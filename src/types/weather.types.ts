f830cb246ac179385826d1f9bd0591e2528f2182
Ahn Jung-min <dkswjdals4027@gmail.com>
2025-11-09 18:26:17 +0900

    날씨 정보 예시

--- src/types/weather.types.ts
+++ src/types/weather.types.ts
@@ -15,3 +15,15 @@ export interface OpenWeatherApiResponse {
     temp_max: number;
   };
 }
+
+/**
+ * 서비스가 프론트엔드에 제공할 가공된 날씨 정보
+ */
+export interface WeatherInfo {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  description: string;
+  icon: string;
+}
