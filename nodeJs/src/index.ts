18a2e404b1471d82cbbb88ba1c59939c7ab395b8
Sungchanyoung <chanyouunng1@gmail.com>
2025-11-12 23:01:29 +0900

    Loads environment variables
    "애플리케이션을 설정하기 위해 dotenv를 사용하여 환경 변수를 로드합니다. 이는 번역 기능과 같이 포트 및 API 키 같은 구성 설정에 접근하기 위해 필요합니다."

--- nodeJs/src/index.ts
+++ nodeJs/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
 import translationRoutes from './routes/translation.route';
+
+// 환경변수 로드
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
