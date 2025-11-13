22f7e4edda24d8acd28476fb7cef06740d216c97
Sungchanyoung <chanyouunng1@gmail.com>
2025-11-13 21:56:35 +0900

    Revert "Merge pull request #1 from SM-KT-G/feature/users"
    This reverts commit 77a54d8dd5be9ff4a219f0aec6d6d642256e727b, reversing
    changes made to 9f963583b37c93d30b8eb9c77b54c5fa7d7e0714.

--- nodeJs/src/index.ts
+++ nodeJs/src/index.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from 'express';
 import apiRoutes from './routes/api';
-import authRoutes from './routes/auth.router';
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,18 +12,10 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express + TypeScript!' });
 });
 
+
 app.use('/api', apiRoutes);
-app.use('/api/auth', authRoutes);
 
-const startServer = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error('Failed to start server:', error);
-    process.exit(1);
-  }
-};
-
-startServer();
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
