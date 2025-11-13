0c446de672b0c3a9e7869018decd732908c72413
Sungchanyoung <chanyouunng1@gmail.com>
2025-11-13 21:55:15 +0900

    일단 커밋

--- nodeJs/src/routes/chatbot.router.ts
+++ nodeJs/src/routes/chatbot.router.ts
@@ -3,5 +3,6 @@ import chatBotController from '../controllers/chatbot.controller';
 
 const chatBotRouter = express.Router();
 
-chatBotRouter.post('/chat', chatBotController.handleChat);
-chatBotRouter.get('/chat/:uuid', chatBotController.getChat);
+chatBotRouter.post('/chat', chatBotController.sendChat); // 채팅을 보낸다
+chatBotRouter.get('/chat/:uuid', chatBotController.getChatByUuid); // 특정 채팅 기록을 불러온다
+chatBotRouter.get('/chats/:userUuid', chatBotController.getChatsByUser); // 특정 사용자의 모든 채팅 기록을 불러온다
