2560c49060a56f758b8080087fd199c384e43c35
Probius-ai <kcfm0529@naver.com>
2025-11-21 22:09:06 +0900

    refactor disaster routes to specify alert retrieval by region, type, and severity

--- nodeJs/src/routes/disaster.router.ts
+++ nodeJs/src/routes/disaster.router.ts
@@ -3,6 +3,10 @@ import DisasterController from '../controllers/disaster.controller';
 
 const disasterRouter = express.Router();
 
-disasterRouter.get('/alert', DisasterController.getAllDisasters);
+disasterRouter.get('/alert/region', DisasterController.getAlertByRegion);
+disasterRouter.get('/alert/type', DisasterController.getAlertByType);
+disasterRouter.get('/alert/severity', DisasterController.getAlertBySeverity);
+
+
 
 export default disasterRouter;
